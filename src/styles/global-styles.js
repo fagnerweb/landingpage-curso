9ea1f202dcce464d515dd3bb860d84f5aa38548a
Fagner Silva <fagnermarketing2014@gmail.com>
2022-10-19 09:15:02 -0300

    creating component heading part 1

--- src/styles/global-styles.js
+++ src/styles/global-styles.js
@@ -6,6 +6,7 @@ export const GlobalStyles = createGlobalStyle`
   * {
       padding: 0;
       margin: 0;
+      box-sizing: border-box;
   }
 
   html {
@@ -14,11 +15,10 @@ export const GlobalStyles = createGlobalStyle`
 
   body {
     font-size: 1.6rem;
-    font-family: ${({ theme }) => theme.fonts.family.default};
+    font-family: ${({ theme }) => theme.font.family.default};
   }
 
-  h1 {
-    font-size: 1.6rem;
-    font-family: ${({ theme }) => theme.fonts.family.secondary};
+  h1, h2, h3, h4, h5, h6 {
+    font-family: ${({ theme }) => theme.font.family.secondary};
   }
 `;
--- src/styles/theme.js
+++ src/styles/theme.js
@@ -4,10 +4,20 @@ export const theme = {
     secondaryColor: '#dc143c',
     white: '#FFFFFF',
   },
-  fonts: {
+  font: {
     family: {
       default: "'Open Sans', sans-serif",
       secondary: "'Montserrat', sans-serif",
+    },
+    sizes: {
+      xsamll: '8rem',
+      small: '1.6rem',
+      medium: '2.4rem',
+      large: '3.2rem',
+      xlarge: '4.0rem',
+      xxlarge: '4.8rem',
+      huge: '5.6rem',
+      xhuge: '6.4rem',
     },
   },
   spacings: {},
