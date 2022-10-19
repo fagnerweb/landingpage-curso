467238c8826713dff7061a4beeb6fe80aea2c2d4
Fagner Silva <fagnermarketing2014@gmail.com>
2022-10-19 10:21:24 -0300

    testing component heading

--- src/components/Heading/index.jsx
+++ src/components/Heading/index.jsx
@@ -25,5 +25,5 @@ Heading.propTypes = {
   colorDark: P.bool,
   as: P.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6']),
   size: P.oneOf(['small', 'medium', 'big', 'huge']),
-  uppercase: P.bool.isRequired,
+  uppercase: P.bool,
 };
--- src/templates/App/App.test.js
+++ src/templates/App/App.test.js
@@ -1,16 +1,8 @@
-import { screen } from '@testing-library/react';
 import { Home } from '.';
 import { renderTheme } from '../../styles/render-theme';
-import { theme } from '../../styles/theme';
 
-test('renders learn react link', () => {
-  renderTheme(<Home />);
-  const headingContainer = screen.getByRole('heading', {
-    name: 'Hello',
-  }).parentElement;
-  expect(headingContainer).toHaveStyle({
-    background: theme.colors.mainBg,
+describe('<Home />', () => {
+  it('should render home', () => {
+    renderTheme(<Home />);
   });
-  expect(headingContainer).toMatchSnapshot();
-  expect(headingContainer).toHaveStyleRule('background', 'red');
 });
