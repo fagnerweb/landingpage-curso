0e1266f1085bd074f71ae34a5689b6d424475de3
Fagner Silva <fagnermarketing2014@gmail.com>
2022-10-19 09:41:40 -0300

    finalizing heading component

--- src/components/Heading/styles.js
+++ src/components/Heading/styles.js
@@ -11,8 +11,16 @@ const titleSize = {
     font-size: ${theme.font.sizes.xlarge};
   `,
   huge: (theme) => css`
-  font-size: ${theme.font.sizes.xhuge};`,
+    font-size: ${theme.font.sizes.xhuge};
+    ${mediaFont(theme)};
+  `,
 };
+
+const mediaFont = (theme) => css`
+  @media ${theme.media.lteMedium} {
+    font-size: ${theme.font.sizes.xlarge};
+  }
+`;
 
 const titleCase = (uppercase) => css`
   text-transform: ${uppercase ? 'uppercase' : 'none'};
