fe65ffb8464c0ad8e2b66f99a8895a325fa3ad07
Fagner Silva <fagnermarketing2014@gmail.com>
2022-10-26 15:21:53 -0300

    api search update, logo fix

--- src/templates/Home/index.jsx
+++ src/templates/Home/index.jsx
@@ -1,9 +1,14 @@
-import { mockBase } from '../Base/mock';
+// import { mockBase } from '../Base/mock';
 import { Base } from '../Base';
 import { useEffect, useState } from 'react';
 import { mapData } from '../../api/map-data';
 import { PageNotFound } from '../PageNotFound';
 import { Loading } from '../Loading';
+
+import { GridTwoColumn } from '../../components/GridTwoColumn';
+import { GridContent } from '../../components/GridContent';
+import { GridImage } from '../../components/GridImage';
+import { GridText } from '../../components/GridText';
 
 export const Home = () => {
   const [data, setData] = useState([]);
@@ -12,7 +17,7 @@ export const Home = () => {
     const load = async () => {
       try {
         const data = await fetch(
-          'http://localhost:1337/api/pages/?filters=[slug]=landing-page&populate[sections][populate]=*',
+          'http://localhost:1337/api/pages/?filters=[slug]=landing-page&populate=deep',
         );
         const json = await data.json();
         const { attributes } = json.data[0];
@@ -34,6 +39,35 @@ export const Home = () => {
   if (data && !data.slug) {
     return <Loading />;
   }
+  const { menu, sections, footerHtml } = data;
+  const { links, text, link, srcImg } = menu;
 
-  return <Base {...mockBase} />;
+  return (
+    <Base
+      links={links}
+      footerHtml={footerHtml}
+      logoData={{ text, link, srcImg }}
+    >
+      {sections.map((section, index) => {
+        const { component } = section;
+        const key = `${index}-${Math.random().toFixed()}`;
+
+        if (component === 'section.section-two-columns') {
+          return <GridTwoColumn key={key} {...section} />;
+        }
+
+        if (component === 'section.section-content') {
+          return <GridContent key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-text') {
+          return <GridText key={key} {...section} />;
+        }
+
+        if (component === 'section.section-grid-image') {
+          return <GridImage key={key} {...section} />;
+        }
+      })}
+    </Base>
+  );
 };
