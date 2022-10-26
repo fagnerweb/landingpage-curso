5af604429f3c360d8316a0704eb6e430cbdde8ab
Fagner Silva <fagnermarketing2014@gmail.com>
2022-10-26 12:28:01 -0300

    created PageNotFound and Loading component, as well as starting the api integration

--- src/templates/App/index.jsx
+++
@@ -1,6 +0,0 @@
-import { mockBase } from '../Base/mock';
-import { Base } from '../Base';
-
-export const Home = () => {
-  return <Base {...mockBase} />;
-};
---
+++ src/templates/Home/index.jsx
@@ -0,0 +1,39 @@
+import { mockBase } from '../Base/mock';
+import { Base } from '../Base';
+import { useEffect, useState } from 'react';
+import { mapData } from '../../api/map-data';
+import { PageNotFound } from '../PageNotFound';
+import { Loading } from '../Loading';
+
+export const Home = () => {
+  const [data, setData] = useState([]);
+
+  useEffect(() => {
+    const load = async () => {
+      try {
+        const data = await fetch(
+          'http://localhost:1337/api/pages/?filters=[slug]=landing-page&populate[sections][populate]=*',
+        );
+        const json = await data.json();
+        const { attributes } = json.data[0];
+        const pageData = mapData([attributes]);
+
+        setData(() => pageData[0]);
+      } catch (e) {
+        setData(undefined);
+      }
+    };
+
+    load();
+  }, []);
+
+  if (data === undefined) {
+    return <PageNotFound />;
+  }
+
+  if (data && !data.slug) {
+    return <Loading />;
+  }
+
+  return <Base {...mockBase} />;
+};
